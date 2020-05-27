e34781baf994f79812ba582e7970b4cbc890e047
intotking <junhee_lee@korea.ac.kr>
2020-05-27 15:55:35 +0900

    [Data]: change structures of example data

--- learning/learning.py
+++ learning/learning.py
@@ -9,13 +9,17 @@ from easyprocess import EasyProcessError
 import time
 import argparse
 from collections import defaultdict 
+import pprint
 
 ROOT_DIR = os.getcwd() # NPEX_Data/.
-model = tree.DecisionTreeClassifier()
-FILE = "model.pkl" 
+model = tree.DecisionTreeRegressor()
+model_filename = "model.pkl" 
 starttime = time.strftime("%m%d_%I%M", time.localtime())
 log = "%s.log" % starttime
-logfile = open("logs/" + log, 'w')
+if os.path.isdir("logs"):
+    logfile = open("logs/" + log, 'w')
+else:
+    logfile = open(log, 'w')
 JAVA_VERSION = 8
 
 def execute(cmd : str, dir):
@@ -49,38 +53,48 @@ def save_model_to_file(model, model_filename: str):
     model_file = open(model_filename, 'wb')
     pickle.dump(model, model_file)
 
-def learning_classifier (data : List[Tuple[List[int], List[List[int]], List[List[int]]]], model):
+def learning_classifier (data : List[Dict], model):
     input_data = []
     labels = []
-    for (program, patches, correctness) in data:
-        for i in range(len(patches)):
-            input_data.append(program + patches[i])
-            labels.append(correctness[i])
+    for patch in data:
+        input_data.append(patch["Encoding"])
+        labels.append(patch["correctness"])
 
     model.fit(input_data, labels) 
     return model
 
-def generate_feature (original_json: Dict, patch_json: Dict):
+def apply_patch_and_generate_feature (original_dir, patch, key_features=None):
+    patch_dir = patch["patch_dir"]
+    original_filepath = patch["path_to_apply"]
+    patch_filepath = "%s/patch.java" % patch_dir
+
+    #1. Apply patch to program (i.e., cp patch.java ...)
+    execute ("cp %s %s.backup" % (original_filepath, patch_filepath), original_dir) # backup
+    execute ("cp %s %s" % (patch_filepath, original_filepath), original_dir)
+    
+    #2. Capture patched program & Generate features
+    execute ("infer capture --java-version %d -- %s" % (JAVA_VERSION, get_compile_command()), original_dir)
+    if key_features is None:
+        execute ("infer npex --error-report %s/npe.json" % patch_dir, original_dir)
+    else:
+        execute ("infer npex --error-report %s/npe.json --key-features %s" % (patch_dir, key_features), original_dir)
+    
+    #3. Backup origianl file
+    execute ("mv %s.backup %s" % (patch_filepath, original_filepath), original_dir) # apply backup
+
+
+
+def generate_feature (original_json: Dict, patch: Dict):
     # Assume: orignal program is buggy version with updated testcases
     repo, commit_id = original_json["repo"], original_json["commit_id"] # 6-digit
     original_dir = "%s/benchmarks/%s/%s" % (ROOT_DIR, repo, commit_id)
     patch_dir = "%s/learning_data/%s/%s" % (ROOT_DIR, repo, commit_id)
     os.chdir (original_dir)
-    if patch_json is None:
+    if patch is None:
         execute ("infer capture --java-version %d -- %s" % (JAVA_VERSION, get_compile_command()), original_dir)
         execute ("infer npex --error-report %s/%s" % (patch_dir, "original_npe.json"), original_dir) 
     else:
-        patch_filepath = "%s/%s" % (patch_dir, patch_json["patch_file"]) 
-        patch_filename = os.path.basename(patch_filepath)
-        #1. Apply patch to program (i.e., cp patch.java ...)
-        file_to_apply_patch = patch_json["path_to_apply"]
-        execute ("cp %s %s.backup" % (file_to_apply_patch, patch_filepath), original_dir) # backup
-        execute ("cp %s %s" % (patch_filepath, file_to_apply_patch), original_dir)
-        #2. Capture patch program (i.e., infer capture ...)
-        execute ("infer capture --java-version %d -- %s" % (JAVA_VERSION, get_compile_command()), original_dir)
-        #3. Analyze and generate features (i.e., infer npex ...)
-        execute ("infer npex --error-report %s/%s" % (patch_dir, patch_json["npe_json"]), original_dir)
-        execute ("mv %s.backup %s" % (patch_filepath, file_to_apply_patch), original_dir) # apply backup
+        apply_patch_and_generate_feature (original_dir, patch)
 
     return read_json_from_file ("infer-out/generated_features.json")
 
@@ -89,7 +103,7 @@ def objective_function (embedded_data):
     label = defaultdict(lambda: ([], []))
     for patch in embedded_data:
         patch_id = str(patch["Id"])
-        patch_vector = str(patch["Concatenated"])
+        patch_vector = str(patch["Encoding"])
         (correct_patches, incorrect_patches) = label[patch_vector]
         if patch["correctness"] is 1:
             label[patch_vector] = (correct_patches + [patch_id], incorrect_patches)
@@ -105,14 +119,14 @@ def objective_function (embedded_data):
         print("%s -> [%d, %d]" % (k, number_of_correct, number_of_incorrect)) 
         number_of_all_data = number_of_all_data + number_of_correct + number_of_incorrect
         score1 = abs(number_of_correct - number_of_incorrect) + score1
-        score2 = (number_of_correct + number_of_incorrect) * (number_of_correct + number_of_incorrect)
+        score2 = (number_of_correct + number_of_incorrect) * (number_of_correct + number_of_incorrect) + score2
     score : float = float(score1/number_of_all_data) - float(score2/(number_of_all_data * number_of_all_data))
-    print(dict(label)) 
+    pprint.pprint(dict(label)) 
     print("Iteration of: %f" % score)
     return score
 
 def embed_data (patch, key_features: List[str]):
-    patch["Concatenated"] = [patch["OriginalFeatures"][k_feature] for k_feature in key_features] + [patch["Features"][k_feature] for k_feature in key_features] 
+    patch["Encoding"] = [patch["OriginalFeatures"][k_feature] for k_feature in key_features] + [patch["Features"][k_feature] for k_feature in key_features] 
 
     return patch
 
@@ -129,7 +143,9 @@ def find_embedding (data: List):
             key_features.append(feature)
             score = score_appended
 
-    print(key_features)
+    with open ("%s/learning/key_feautures" % ROOT_DIR, 'w') as file:
+        file.write('\n'.join(key_features))
+    print('\n'.join(key_features))
     print("Score : %s" % score)
 
     return key_features
@@ -144,10 +160,9 @@ def data_from_DB ():
         original["Features"] = generate_feature (original, None)
 
         #TODO: add devel patch 
-        patch_json_filepaths = glob.glob("%s/patch*[0-9].json" % err_info_dir)
-        print (patch_json_filepaths)
-        for patch_json_filepath in patch_json_filepaths:
-            patch = read_json_from_file (patch_json_filepath) 
+        for patch_dir in [ dir for dir in glob.glob("%s/*" % err_info_dir) if os.path.isdir(dir)]:
+            patch = read_json_from_file ("%s/patch.json" % patch_dir)
+            patch["patch_dir"] = patch_dir
             patch["Features"] = generate_feature (original, patch)
             patch["OriginalFeatures"] = original["Features"] 
             patch["Id"] = "%s_%s_%s" % (original["repo"], original["commit_id"], patch["patch_file"])
@@ -155,13 +170,28 @@ def data_from_DB ():
             ret.append(patch)
     return ret
 
+def scoring (learning_data, model):
+    score = 0
+    score2 = 0
+    input_data = [patch["Encoding"] for patch in learning_data]
+    result = model.predict(input_data)
+    for i in range(len(input_data)):
+        learning_data[i]["score"] = 1 - abs(result[i] - learning_data[i]["correctness"]) 
+        score2 = score2 + learning_data[i]["score"]
+        if learning_data[i]["score"] > 0.5:
+            score = score + 1
+
+    print ("Score on learning data : %f" % float(score/len(learning_data)))
+    print ("Score2 on learning data : %f" % score2/len(learning_data))
+    return learning_data
+
 if __name__ == '__main__':
 ##  Data for testing
     data = data_from_DB()
-    find_embedding(data)
-    #filter = find_filter (data)
-    #data = apply_filter (data, filter)
-    model = learning_classifier (data, model) 
-    save_model_to_file (model, FILE)
+    key_features = find_embedding(data)
+    learning_data = [embed_data (patch, key_features) for patch in data]
+    model = learning_classifier (learning_data, model) 
+    save_model_to_file (model, model_filename)
+    scoring (learning_data, model)
     #saver filter 
 
---
+++ learning_data/sequential_deref/2/2/patch.java
@@ -0,0 +1,65 @@
+class D {
+  int data;
+}
+
+class C {
+  int data;
+  public D h() {
+    D ret = new D();
+    ret.data = this.data;
+    return ret;
+  }
+}
+class B {
+  int data;
+  public C g() {
+    C ret = new C();
+    ret.data = this.data;
+    return ret;
+  }
+}
+
+class A {
+  int data;
+  public B f() {
+    B ret = new B();
+    ret.data = this.data;
+    return ret;
+  }
+}
+
+class X {
+  int data;
+  public A null_src(boolean b) {
+    if (b)
+      return null;
+    else
+      return new A();
+  }
+  public void store(D d) {
+    this.data = d.data;
+  }
+  public void print() {
+    System.out.println("HIHI");
+  }
+}
+
+
+
+class Main {
+  public static void main (String args[])
+  {
+    X x = new X();
+    A a = x.null_src(false);
+    if (a != null) {
+    B b = a.f();
+    C c = b.g();
+    D d = c.h();
+    
+    x.store(d);
+    }
+    x.print();
+  }
+}
+
+
