a6379ba2b60527ad82bf1c33ceaba96393841142
intotking <junhee_lee@korea.ac.kr>
2020-10-03 22:21:29 +0900

    Bears-88-buggy

---
+++ target/generated-test-sources/test-annotations/org/springframework/data/querydsl/QAddress.java
@@ -0,0 +1,41 @@
+package org.springframework.data.querydsl;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAddress is a Querydsl query type for Address
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QAddress extends EntityPathBase<Address> {
+
+    private static final long serialVersionUID = 1091641720L;
+
+    public static final QAddress address = new QAddress("address");
+
+    public final StringPath city = createString("city");
+
+    public final ArrayPath<Double[], Double> lonLat = createArray("lonLat", Double[].class);
+
+    public final StringPath street = createString("street");
+
+    public QAddress(String variable) {
+        super(Address.class, forVariable(variable));
+    }
+
+    public QAddress(Path<? extends Address> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAddress(PathMetadata metadata) {
+        super(Address.class, metadata);
+    }
+
+}
+
