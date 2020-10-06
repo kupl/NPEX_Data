a3420635e26f55fee132251f79fd9fab908f70eb
intotking <junhee_lee@korea.ac.kr>
2020-10-06 20:37:42 +0900

    fix fixed version

--- src/main/java/org/org/apache/commons/math3/geometry/euclidean/twod/PolygonsSet.java
+++ src/main/java/org/org/apache/commons/math3/geometry/euclidean/twod/PolygonsSet.java
@@ -229,6 +229,10 @@ public class PolygonsSet extends AbstractRegion<Euclidean2D, Euclidean1D> {
     private static Line[] boxBoundary(final double xMin, final double xMax,
                                       final double yMin, final double yMax,
                                       final double tolerance) {
+	 			if ((xMin >= xMax - tolerance) || (yMin >= yMax - tolerance)) {
+             // too thin box, build an empty polygons set
+             return null;
+         }
         final Vector2D minMin = new Vector2D(xMin, yMin);
         final Vector2D minMax = new Vector2D(xMin, yMax);
         final Vector2D maxMin = new Vector2D(xMax, yMin);
